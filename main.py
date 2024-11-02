e02124b1d9b8d70a735d02eca09aec15a2f5d908
Carlo Hildebrandt <github@carlo-hildebrandt.de>
2024-11-02 15:29:06 +0100

    use non-standard port

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 app.include_router(Controller.router)
 
 def main() -> None:
-    uvicorn.run("main:app", host="0.0.0.0")
+    uvicorn.run("main:app", host="0.0.0.0", port=8001)
 
 
 if __name__ == "__main__":
